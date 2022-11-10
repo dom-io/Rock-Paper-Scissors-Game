dc3a4c1cd518c0a6ef117aeeebcc6137478fff5c
Dom <111844962+dom-io@users.noreply.github.com>
2022-11-10 00:41:53 -0800

    I have added a very basic css file and have added buttons and DOM manipulation to pass the values into the clickFunction so the buttons and scores work. I have updated the html to include basic divs for the bare bones of the game.

--- game.js
+++ game.js
@@ -3,49 +3,69 @@ let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
 
+const rockBtn = document.querySelector('.rock-btn');
+const paperBtn = document.querySelector('.paper-btn');
+const scissorsBtn = document.querySelector('.scissors-btn');
+const playerScoreElement = document.querySelector('.player-score');
+const computerScoreElement = document.querySelector('.computer-score');
+const msgElement = document.querySelector('.round-msg');
+
 // Creates the computer's random choice
 const computerChoice = function () {
   let randomComputerChoice = Math.floor(Math.random() * 3);
-  if (randomComputerChoice == "0") {
-    return "rock";
-  } else if (randomComputerChoice == "1") {
-    return "paper";
+  if (randomComputerChoice == '0') {
+    return 'rock';
+  } else if (randomComputerChoice == '1') {
+    return 'paper';
   } else {
-    return "scissors";
+    return 'scissors';
   }
 };
 
 // Plays one round
 const playRound = function (playerSelection, computerSelection) {
-  playerSelection = prompt("Choose Rock, Paper, or Scissors!");
-  playerSelection = playerSelection.toLowerCase();
+  // playerSelection = prompt('Choose Rock, Paper, or Scissors!');
+  // playerSelection = playerSelection.toLowerCase();
   computerSelection = computerChoice();
   if (playerSelection === computerSelection) {
     return "It's a draw!";
   } else if (
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "rock")
+    (playerSelection == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper') ||
+    (playerSelection == 'paper' && computerSelection == 'rock')
   ) {
     playerScore++;
-    return "You have won! 👍";
+    return 'You have won! 👍';
   } else if (
-    (computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "scissors" && playerSelection == "paper") ||
-    (computerSelection == "paper" && playerSelection == "rock")
+    (computerSelection == 'rock' && playerSelection == 'scissors') ||
+    (computerSelection == 'scissors' && playerSelection == 'paper') ||
+    (computerSelection == 'paper' && playerSelection == 'rock')
   ) {
     computerScore++;
-    return "The computer has won! 👎";
+    return 'The computer has won! 👎';
   }
 };
 
 // Loops through the game 5 times
-const game = function () {
-  for (let i = 1; i < 6; i++) {
-    console.log(playRound());
-  }
-};
+// const game = function () {
+//   for (let i = 1; i < 6; i++) {
+//     console.log(playRound());
+//   }
+// };
 
-game();
-console.log(playerScore);
-console.log(computerScore);
+// game();
+
+// Restart game TO DO
+
+rockBtn.addEventListener('click', () => clickFunction('rock'));
+paperBtn.addEventListener('click', () => clickFunction('paper'));
+scissorsBtn.addEventListener('click', () => clickFunction('scissors'));
+
+function clickFunction(playerSelection) {
+  if (playerScore <= 5 || computerScore <= 5) {
+    const computerSelection = computerChoice;
+    playRound(playerSelection, computerSelection);
+    playerScoreElement.textContent = playerScore;
+    computerScoreElement.textContent = computerScore;
+  }
+}
